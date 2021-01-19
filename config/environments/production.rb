f9869be3cabbfd52b2d9249493ecf2032f1b7105
Scott Fleming <sdflem@gmail.com>
2021-01-19 10:53:46 -0600

    Replace demo-n-deets name with practice-app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails_demos_n_deets_2021_app_production"
+  # config.active_job.queue_name_prefix = "practice_app_production"
 
   config.action_mailer.perform_caching = false
 
